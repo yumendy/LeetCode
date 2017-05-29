4c2a1d2d41e49635d1154623ffd1ce1ab243923c
yumendy <yumendy@163.com>
2017-05-29 20:13:47 +0800

    2017-5-29

--- Python/Number of 1 Bits.py
+++ Python/Number of 1 Bits.py
@@ -1,10 +1,11 @@
-class Solution:
-    # @param n, an integer
-    # @return an integer
+class Solution(object):
     def hammingWeight(self, n):
-        count = 0
-        while n > 0:
-            if (n & 1) is not 0:
-                count += 1
-            n >>= 1;
-        return count
+        """
+        :type n: int
+        :rtype: int
+        """
+        res = 0
+        while n:
+            res += 1
+            n &= (n - 1)
+        return res
